eb87f65485c3b5fe6617248ac0eb6b5107ed992b
kxowi <karim-xowi@live.fr>
2023-02-16 07:22:30 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-/*
+/**
  * main - Aprogram that  prints the size of various computer types 
  * return 0 (Success)
  */
-
 int main(void)
 {
 	char a;
@@ -19,4 +18,3 @@ printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
-
